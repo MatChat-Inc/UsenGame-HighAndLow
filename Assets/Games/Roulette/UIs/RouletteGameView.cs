3bc8f2ca268b357a5828a77faa4a1dbac1042eb3
LunarEclipse <949886@qq.com>
2024-12-25 09:11:23 +0800

    fix: adjust bottom buttons

--- Assets/Games/Roulette/UIs/RouletteGameView.cs
+++ Assets/Games/Roulette/UIs/RouletteGameView.cs
@@ -177,7 +177,6 @@ namespace USEN.Games.Roulette
             confirmText.text = "停止";
             
             // Hide bottom buttons
-            bottomPanel.confirmButton.gameObject.SetActive(false);
             bottomPanel.yellowButton.gameObject.SetActive(false);
             bottomPanel.blueButton.gameObject.SetActive(false);
             bottomPanel.redButton.gameObject.SetActive(false);
@@ -218,6 +217,8 @@ namespace USEN.Games.Roulette
             
             // Play sfx
             SFXManager.PlayRepeatedly(R.Audios.SfxRouletteGameRotating);
+            
+            bottomPanel.confirmButton.gameObject.SetActive(false);
         }
         
         private async Task StopWheel()
